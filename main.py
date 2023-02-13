d2ba700494c0708341e12c04d57d80a79f395607
alekseymrvsk <alekseymrvsk@yandex.by>
2023-02-13 16:53:25 +0300

    fix bugs

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ NUMBER_COLUMNS_TRAIN = 43
 INPUT_FILE_FORMAT = '.csv'
 
 
+# For checking train dataset set check_train True, for checking test dataset - False
 def check_dataset(file_path, check_train=True):
     dataset = pd.read_csv(filepath_or_buffer=file_path)
     if check_train:
@@ -30,7 +31,7 @@ def check_dataset(file_path, check_train=True):
             return False
 
 
-@app.post("/predict")
+@app.get("/predict")
 def predict_data(INPUT_TRAIN: UploadFile, INPUT_TEST: UploadFile):
     if not INPUT_TRAIN.filename.lower().endswith('.csv'):
         return 404, "Please upload csv  file."
@@ -54,9 +55,18 @@ def predict_data(INPUT_TRAIN: UploadFile, INPUT_TEST: UploadFile):
 
     predict = model.predict_data(filepath)
 
-    metric = model.get_metric()
+    #metric = model.get_metric()
     df = pd.DataFrame(predict)
     df.columns = ["Prediction"]
     df.to_csv("output_data/prediction.csv", index_label="Id")
 
     return FileResponse("output_data/prediction.csv", filename="prediction", media_type='text/csv')
+
+# Client code
+# import requests
+#
+# url = 'http://127.0.0.1:8000/predict'
+# files = {'INPUT_TRAIN': open('data/train.csv', 'rb'), 'INPUT_TEST': open('data/test.csv', 'rb')}
+# resp = requests.get(url=url, files=files, allow_redirects=True)
+# with open("response.csv", "w") as f:
+#     f.write(resp.text)
